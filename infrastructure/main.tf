c829c3456493b21f3f1cb7dfb9272f87c1e5794e
AmirHossam <amirhossameldien@gmail.com>
2023-12-10 22:08:57 +0200

    Adding The infrastructure and the Deploy stage on Travis-CI

---
+++ infrastructure/main.tf
@@ -0,0 +1,103 @@
+#Create Instance Profile
+resource "aws_iam_instance_profile" "ec2_eb_profile" {
+  name = "docker-ec2-profile"
+  role = aws_iam_role.ec2_role.name
+}
+
+resource "aws_iam_role" "ec2_role" {
+  name = "docker-ec2-role"
+  assume_role_policy = data.aws_iam_policy_document.assume_policy.json
+  managed_policy_arns = [
+    "arn:aws:iam::aws:policy/AWSElasticBeanstalkWebTier",
+    "arn:aws:iam::aws:policy/AWSElasticBeanstalkMulticontainerDocker",
+    "arn:aws:iam::aws:policy/AWSElasticBeanstalkWorkerTier",
+    "arn:aws:iam::aws:policy/EC2InstanceProfileForImageBuilderECRContainerBuilds"
+  ]
+
+  inline_policy {
+    name   = "eb-application-permissions"
+    policy = data.aws_iam_policy_document.permissions.json
+  }
+}
+
+
+#Create elasticbeanstalk application
+resource "aws_elastic_beanstalk_application" "docker-app" {
+  name = "my-docker-app"
+  description = "React app deployment"
+}
+
+#Create eb environment
+resource "aws_elastic_beanstalk_environment" "eb_env" {
+  name = "EB-env"
+  application = aws_elastic_beanstalk_application.docker-app.name
+  platform_arn = "arn:aws:elasticbeanstalk:us-west-2::platform/Docker running on 64bit Amazon Linux 2023/4.1.1"
+  cname_prefix = aws_elastic_beanstalk_application.docker-app.name
+  
+  setting {
+    namespace = "aws:autoscaling:launchconfiguration"
+    name = "IamInstanceProfile"
+    value = aws_iam_instance_profile.ec2_eb_profile.name
+  }
+  
+  setting {
+    namespace = "aws:elasticbeanstalk:environment"
+    name      = "LoadBalancerType"
+    value     = "application"
+  }
+  
+  setting {
+    namespace = "aws:elasticbeanstalk:environment"
+    name      = "EnvironmentType"
+    value     = "SingleInstance" # Use "LoadBalanced" for multiple instances
+  }
+
+  setting {
+    namespace = "aws:autoscaling:launchconfiguration"
+    name      = "InstanceType"
+    value     = "t2.micro" # Adjust instance type as needed
+  }
+
+  setting {
+    namespace = "aws:elasticbeanstalk:environment"
+    name      = "ServiceRole"
+    value     = "aws-elasticbeanstalk-service-role" # Provide a custom service role if necessary
+  }
+
+  setting {
+    namespace = "aws:elasticbeanstalk:healthreporting:system"
+    name      = "SystemType"
+    value     = "enhanced" # Choose between basic or enhanced health reporting
+  }
+  
+  setting {
+    namespace = "aws:elasticbeanstalk:environment:process:default"
+    name      = "MatcherHTTPCode"
+    value     = 200
+  }
+  
+  setting {
+    namespace = "aws:elasticbeanstalk:environment:process:default"
+    name      = "HealthCheckPath"
+    value     = "/docs"
+  }
+  
+  setting {
+    namespace = "aws:ec2:vpc"
+    name      = "ELBScheme"
+    value     = "internet facing"
+  }
+
+  setting {
+    namespace = "aws:ec2:vpc"
+    name      = "VPCId"
+    value     = "vpc-0e38fb0d8112f48fe"
+  }
+
+  setting {
+    namespace = "aws:ec2:vpc"
+    name      = "Subnets"
+    value     = "subnet-08e8b8498454f1dd0"
+  }
+}
+
